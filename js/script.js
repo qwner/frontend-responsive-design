5179e2bc6bbb0e37e46f11d5f5b06b39bbd7b498
Hakan K <hakan96kaplan@gmail.com>
2020-01-29 20:20:36 +0100

    changes to header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 
+// var body = document.querySelector('body')
+// var button = document.querySelector('body header button')
+// var nav = document.querySelector('body header nav')
+
+// function toggleMenu(){
+// nav.classList.toggle('showMe')
+// }
+
+// button.addEventListener('click', toggleMenu)
+
+var nav = document.querySelector('header nav')
+var button = document.querySelector('header button')
+
+function toggleMenu(){
+nav.classList.toggle('showMe')
+}
+
+button.addEventListener('click', toggleMenu)
