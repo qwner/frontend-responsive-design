43b34f518f36c23da129ab0ad697db33c2b1af5f
Hakan K <hakan96kaplan@gmail.com>
2020-01-30 23:07:08 +0100

    grid beter toegepast

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-
-// var body = document.querySelector('body')
-// var button = document.querySelector('body header button')
-// var nav = document.querySelector('body header nav')
-
-// function toggleMenu(){
-// nav.classList.toggle('showMe')
-// }
-
-// button.addEventListener('click', toggleMenu)
-
 var nav = document.querySelector('header nav')
 var button = document.querySelector('header button')
 
+
 function toggleMenu(){
-nav.classList.toggle('showMe')
+    nav.classList.toggle('showMe')
 }
 
 button.addEventListener('click', toggleMenu)
