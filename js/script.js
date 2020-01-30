f751201e47e697febbaa3511d939fb35cbe2a2d4
Hakan K <hakan96kaplan@gmail.com>
2020-01-31 00:04:58 +0100

    meer uitleg in CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// samen met David gemaakt. Hier maak ik variabelen aan die gekoppeld worden aan de nav en button
 var nav = document.querySelector('header nav')
 var button = document.querySelector('header button')
 
-
+// hier wordt een class aangemaakt wanneer je op button klikt
 function toggleMenu(){
     nav.classList.toggle('showMe')
 }
